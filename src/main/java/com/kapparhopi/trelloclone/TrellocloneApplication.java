ef19acc06a08f6ba4f775496077fc6b77abc1e73
Maxime Drouin <chicoboymax@gmail.com>
2018-11-01 04:27:13 -0400

    First commit

---
+++ src/main/java/com/kapparhopi/trelloclone/TrellocloneApplication.java
@@ -0,0 +1,12 @@
+package com.kapparhopi.trelloclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrellocloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrellocloneApplication.class, args);
+    }
+}
