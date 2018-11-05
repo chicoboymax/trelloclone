7e45ed8672cba68cf92a0c6df76e2ef2e4e1a03b
Maxime Drouin <chicoboymax@gmail.com>
2018-11-05 04:27:18 -0500

    Added find all method

--- src/main/java/com/kapparhopi/trelloclone/web/ProjectController.java
+++ src/main/java/com/kapparhopi/trelloclone/web/ProjectController.java
@@ -40,4 +40,9 @@ public class ProjectController {
         Project project = projectService.findProjectByItentifier(projectId);
         return new ResponseEntity<>(project, HttpStatus.OK);
     }
+
+    @GetMapping("/all")
+    public Iterable<Project> getAllProjects() {
+        return projectService.findAllProjects();
+    }
 }
